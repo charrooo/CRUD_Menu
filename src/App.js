238860d50924b8bf85a9586c0e59e75dafbd7f23
Christian <ybanezchristiansharo@gmail.com>
2024-06-09 09:26:11 +0800

    functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Write from "./components/Write";
+import Read from "./components/Read";
+import UpdateRead from "./components/UpdateRead";
+import UpdateWrite from "./components/UpdateWrite";
 
 function App() {
   return (
     <div className="App">
-      <div>Hello</div>
+      <Router>
+        <Routes>
+        <Route path="/" element={ <Write />} />
+        <Route path="/write" element={ <Write />} />
+        <Route path="/read" element={ <Read />} />
+          <Route path="/updateread" element={ <UpdateRead />} />
+          <Route path="/updatewrite/:firebaseId" element={ <UpdateWrite /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
